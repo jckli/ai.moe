8cfd9d8cd5f5c48e7fdb741db3ce802996df2bb8
Jack Li <jack@jackli.dev>
2022-04-06 16:16:08 -0700

    chore: cleanup lines

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ["scontent-lax3-2.cdninstagram.com", "scontent-lax3-1.cdninstagram.com"],
-  },
 }
--- src/components/InstagramPost.tsx
+++ src/components/InstagramPost.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export const InstagramPost = (props: any) => {
     return (
         <div className="mb-6">
